99f98e4c7e8c6a5dd905ffb94164f08cdeb98e91
Alfredo Mendieta <u201000281@gmail.com>
2012-08-05 19:15:06 -0500

    first commit

---
+++ DEW_Twiter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWTwiter::Application.config.secret_token = 'e97d9f3d042f5d1a3f6a6e1385594b938b9eb77993a5ee0a222550d85a4d37ae09c5b0d51fa6a84ed69fbfd7dfdab308c4a97714c637783b934d828db7aea3e4'
